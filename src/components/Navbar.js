97e24cc25567f0c12c8b03a2333f34c6c48b8516
Areeb Ahmed <ahmedareeb401@gmail.com>
2022-10-15 17:11:07 +0300

    Logo and navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,15 @@ import React from 'react'
 import './navbar.css'
 import { Link } from 'react-router-dom'
 import '../App.css'
+import logodark from '../Assets/logodark.png'
+import logolight from '../Assets/logolight.png'
 
 const Navbar = () => {
   return (
 
     <div className="parent">
       <nav className="navbar">
+        <img src={logodark} alt='logo' width ='100px'></img>
         <ul>
           <li><Link className='navLink' to='/'>Home</Link></li>
           <li>|</li>
