88b960f20ede8e122a62f69d186975c4364a058e
Umair Malik <umairmalik7777777@gmail.com>
2022-10-16 21:43:56 +0300

    Home page

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import './home.css'
 import '../../App.css'
-import panel from '../../Assets/panel.png'
+import panel from '../../Assets/panel2.png'
 import Typewriter from 'typewriter-effect';
 import { Link } from "react-router-dom";
+import toolkitpic from '../../Assets/toolkitpic.png'
 
 const home = () => { 
 
@@ -19,10 +20,10 @@ const home = () => {
           }} 
           onInit={(typewriter) => {
             typewriter
-            .typeString("<span style='color:var(--color3);'>Welcome to Solar Photovoltaic System Design and Installation Toolkit!</span>")
+            .typeString("<span style='color:var(--color3);'>Welcome to a Solar Photovoltaic System Design and Installation Toolkit!</span>")
             .pauseFor(1000)
             .deleteAll()
-            .typeString("<span style='color:red;'>Developed by the students from the University of Nairobi</span>")
+            .typeString("<span style='color:red;'>Developed by students from the University of Nairobi</span>")
             .pauseFor(1000)
             .start();
           }}/>
@@ -32,8 +33,17 @@ const home = () => {
         <div className='infoleft'>
           <h1>What equipment is required?</h1>
           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum vitae odit alias labore deserunt temporibus voluptatem ex sequi repudiandae sapiente. Sit nisi nam sapiente tenetur maxime aspernatur itaque beatae facilis pariatur illum. Voluptate maxime quam dicta dolorum exercitationem iure culpa voluptatum. Sint neque porro nemo esse architecto ea blanditiis temporibus.</p>
-          <Link className='infolink' to='/info'><button className='infobtn' >Know More...</button></Link>
+          <Link className='infolink' to='/info'><button className='btn' >Learn More</button></Link>
         </div>        
+      </div>
+
+      <div className='toolkit'>
+          <img src={toolkitpic} alt="toolkitpic" />
+        <div className='toolkitright'>
+          <h1>Easy to use Toolkit</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, ab maxime? Aliquam id, quis repellat cum esse consequatur eligendi iusto nisi rem? Sit autem ea, animi explicabo fugit minus quas maiores repellat facere illo nisi officiis provident deserunt, saepe non et dignissimos tempore ut cupiditate voluptas temporibus nam, accusamus tempora.</p>
+          <Link className='toolkitlink' to='/toolkit'><button className='btn' >Start</button></Link>
+        </div>
       </div>
     </body>
     
