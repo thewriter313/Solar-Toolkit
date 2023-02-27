19b1e71bc9473f8fa5044d4ba55e34ffaf0cc837
Areeb Ahmed <ahmedareeb401@gmail.com>
2023-02-27 19:13:37 +0300

    fix updated

--- src/pages/toolkit/toolkit.jsx
+++ src/pages/toolkit/toolkit.jsx
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import  React,{ useState } from "react";
 import GoToTop from "../../components/GoToTop";
 import { Stepper } from "react-form-stepper";
 import "./toolkit.css";
