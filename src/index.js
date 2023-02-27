1be66cc159c4349886bd3bf18e01a91dd049e227
Areeb Ahmed <ahmedareeb401@gmail.com>
2023-02-27 20:06:54 +0300

    UPDATED fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-// import ReactDOM from "react-dom";
-import { createRoot } from 'react-dom/client';
+import ReactDOM from "react-dom/client";
+// import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/layout";
 import About from './pages/about/about'
@@ -27,5 +27,5 @@ export default function App() {
 
 // ReactDOM.render(<App />, document.getElementById("root"));
 const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const root = ReactDOM.createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(<App/>);
