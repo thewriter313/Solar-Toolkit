5da3f758604b1915f840f17a9e31a4d8c62e6216
Areeb Ahmed <ahmedareeb401@gmail.com>
2023-03-26 16:45:38 +0300

    pics resized and added to about page

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -2,9 +2,9 @@ import React, { useEffect } from "react";
 import "./about.css";
 import Form from "../../components/Form";
 import GoToTop from "../../components/GoToTop";
-import umair from "../../Assets/umair.png";
-// import thomas from "../../Assets/default.png";
-import areeb from "../../Assets/areeb.png";
+// import umair from "../../Assets/umair.png";
+import thomas from "../../Assets/default.png";
+// import areeb from "../../Assets/areeb.png";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { IoMdQuote } from "react-icons/io";
@@ -51,7 +51,7 @@ const About = () => {
                 <div
                     data-aos="fade-up"
                     className="about-section-left flexcolumn">
-                    <img src={umair} alt="areeb" />
+                    <img src={thomas} alt="areeb" />
                 </div>
             </div>
 
@@ -62,7 +62,7 @@ const About = () => {
                 <div
                     data-aos="fade-up"
                     className="about-section-right flexcolumn">
-                    <img src={areeb} alt="umair" />
+                    <img src={thomas} alt="umair" />
                 </div>
                 <div
                     data-aos="fade-up"
