914d965a79304013d38c323714cea35ec07daaba
thewriter313 <umairmalik7777777@gmail.com>
2023-03-29 13:51:03 +0300

    Small CHanges 3

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -10,8 +10,8 @@ import { IoMdQuote } from "react-icons/io";
 
 const About = () => {
     useEffect(() => {
-        AOS.init({ duration: 1500 })
-    }, [])
+        AOS.init({ duration: 1500 });
+    }, []);
 
     return (
         <div className="about-page flexcolumn">
@@ -27,18 +27,33 @@ const About = () => {
                         <div>
                             <h1 className="introHead">Hello</h1>
                             <p>
-                                I'm Umair A. Malik, a 23-year-old <span style={{color: "var(--color3)"}}>Mechanical
-                                Engineering</span> student at the University of Nairobi.
-                                In my final year of study, I have worked on this solar
+                                I'm Umair A. Malik, a 23-year-old{" "}
+                                <span style={{ color: "var(--color3)" }}>
+                                    Mechanical Engineering
+                                </span>{" "}
+                                student at the University of Nairobi. In my
+                                final year of study, I have worked on this solar
                                 home system sizing toolkit to help homeowners
-                                determine the right <span style={{color: "var(--color3)"}}>size of solar equipment</span> for
-                                their homes. This tool is essential as it can save
-                                resources and expenses. I'm interested in promoting
-                                <span style={{color:"var(--color3)"}}> sustainable energy solutions</span> and contributing to a
-                                <span style={{color:"var(--color3)"}}> greener environment</span>.
-                        </p>
+                                determine the right{" "}
+                                <span style={{ color: "var(--color3)" }}>
+                                    size of solar equipment
+                                </span>{" "}
+                                for their homes. This tool is essential as it
+                                can save resources and expenses. I'm interested
+                                in promoting
+                                <span style={{ color: "var(--color3)" }}>
+                                    {" "}
+                                    sustainable energy solutions
+                                </span>{" "}
+                                and contributing to a
+                                <span style={{ color: "var(--color3)" }}>
+                                    {" "}
+                                    greener environment
+                                </span>
+                                .
+                            </p>
                         </div>
-                        
+
                         <div className="flexrow">
                             <IoMdQuote
                                 className="endQuote"
@@ -73,18 +88,25 @@ const About = () => {
                         <div>
                             <h1 className="introHead">Hello</h1>
                             <p>
-                                I'm Areeb Ahmed, a 23-year-old <span style={{color: "var(--color3)"}}>Mechanical
-                                Engineering</span> student at the University of Nairobi.
-                                In my final year of study, I have worked on this solar
-                                home system sizing toolkit to help homeowners
-                                determine the right <span style={{color: "var(--color3)"}}>size of solar equipment</span> for
-                                their homes. This tool is essential as it can save
-                                resources and expenses. I'm interested in promoting
-                                <span style={{color:"var(--color3)"}}> sustainable energy solutions</span> and contributing to a
-                                <span style={{color:"var(--color3)"}}> greener environment</span>.
-                        </p>
+                                My name is Areeb Ahmed. I'm a 23-year-old{" "}
+                                <span style={{ color: "var(--color3)" }}>
+                                    Mechanical Engineering
+                                </span>{" "}
+                                student in my final year at the University of
+                                Nairobi. I believe that access to clean and
+                                renewable energy is essential for communities
+                                around the world, and our solar toolkit is a
+                                small step towards making that a reality. With
+                                the Hel
+                                <span style={{ color: "var(--color3)" }}>
+                                    EOS
+                                </span>{" "}
+                                toolkit, we hope to empower individuals and
+                                communities to harness solar energy and make a
+                                positive impact on the environment.
+                            </p>
                         </div>
-                        
+
                         <div className="flexrow">
                             <IoMdQuote
                                 className="endQuote"
